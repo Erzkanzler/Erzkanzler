70717b782264aa769c9bcf397e8c36968d09fb3c
Your Name <solodovnikovdenis@gmail.com>
2018-09-15 15:39:47 +0300

    Все еще ошибка, начинаю новый файл

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import urllib.request
 from bs4 import BeatifulSoup
 import csv
 
-Base_url = 'htpp://weblacner.net/projects/''
+Base_url = 'htpp://weblacner.net/projects/'
 
 
 def get_html(url):
@@ -12,18 +12,18 @@ def get_html(url):
 
 def get_page_count(html):
     soup = BeatifulSoup(html)
-    pagenation soup.find('div', class_="pages_list text_box")
+    pagenation = soup.find('div', class_="pages_list text_box")
     return int(pagenation.find_all('a')[-2.].text)
 
 
 def parse(html):
     Soup = BeatifulSoup(html)
-    table = Soup.find('table', class_'items_list')
+    table = Soup.find('table', class_='items_list')
 
     projects.append({
-        'title': cols[0].a.text
-        'categories': [categories.text for categories in cals[0].div.find_all('noindex')]
-        'price': cols[1].text.strip()
+        'title': cols[0].a.text,
+        'categories': [category.text for category in cols[0].div.find_all('noindex')],
+        'price': cols[1].text.strip(),
         'application': cols[2].text.strip().split()[0]
     })
 
@@ -33,7 +33,7 @@ def parse(html):
 def save(projects, path):
     with open(path, 'w') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(("Проект", "Категории", "Цена", "Заявки"))
+        writer.writerow(("Project", "categories", "Cena", "Zayavki"))
 
         for projects in projects:
             writer.writerow((projects['tittle'], ', '.join(projects['categories'], projects['price'], projects['application']))
@@ -41,6 +41,7 @@ def save(projects, path):
 
 def main():
     page_count=get_page_count(get_html(Base_url))
+
     print('Всего найдено страниц %d' % page_count)
 
     projects[]
